deb04cee997e8bfa621f5f149bd3ce019784c35e
Iván Gabriel <tocutoeltuco@gmail.com>
2020-01-06 20:53:17 -0300

    [build] [host] Modify the help text

--- translations/global/en.lua
+++ translations/global/en.lua
@@ -8,7 +8,7 @@ Use [<d>!help</d>] to get more information.
 	lang_set = "<j>Module language set to: [<d>English (en)</d>].",
 	current_lang = "<j>Current module language: [<d>English (en)</d>].",
 
-	help = "<vp>What is <b>Tocu Laboratories</b>?\n<j><d><b>Tocu Laboratories</b></d> is a module, hosted by <d><b>Tocutoeltuco#0000</b></d> used to publish and test stuff. You can see the project/submode list by typing <d><b>!modes</b></d> in the chat.\n<vp>Can I host something in the module?\n<j>Of course you can. You need to go to our github repository (<d><b>%s</b></d>), fork it and add your module here. In the README file you'll have all the information.\n<vp>What are the available commands?\n<j>You can use the <d><b>!commands</b></d> command to see them.",
+	help = "<vp>What is <b>Tocu Laboratories</b>?\n<j><d><b>Tocu Laboratories</b></d> is a module hosted by <d><b>Tocutoeltuco#0000</b></d>, used to publish and test stuff. You can see the project/submode list by typing <d><b>!modes</b></d> in the chat.\n<vp>Can I host something in the module?\n<j>Of course you can. You need to go to our github repository (<d><b>%s</b></d>), fork it and add your module here. In the README file you'll have all the information.\n<vp>What are the available commands?\n<j>You can use the <d><b>!commands</b></d> command to see them.",
 	use_info = "<d>Tip: Use [<j>!info</j>] to get information about this submode.</d>",
 	help_command_help = "Shows information about Tocu Laboratories. You can also use [<d>!help [command]</d>] to get more information about a command.",
 	help_command_modes = "Shows available submodes.",
