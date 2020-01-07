926ab6e3045324ac5729d6d34e2a00eae0f11abe
Iván Gabriel <tocutoeltuco@gmail.com>
2020-01-07 16:17:12 -0300

    [build] [host] Little stream bugfix
    Basically, the link of the first video had a ! on the start, product of the streaming system.

--- youtube-searcher/backend.lua
+++ youtube-searcher/backend.lua
@@ -14,7 +14,7 @@ function on_stream_answer(answer, answer_id)
 		state = 2
 
 		videos._count = 0
-		for id, title, author, desc in string.gmatch(answer, "([^#]+)#([^#]+)#([^#]+)#([^#]+)#") do
+		for id, title, author, desc in string.gmatch(string.sub(answer, 2), "([^#]+)#([^#]+)#([^#]+)#([^#]+)#") do
 			videos._count = videos._count + 1
 			videos[videos._count] = {
 				id = deserialize(id),
