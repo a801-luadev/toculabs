6432ca5ca57e8e773d559d1a35eca7aee3ff289f
Sergiu Bucur <admin@b1sergiu.online>
2020-01-09 20:05:39 +0200

    Removed those two lines

--- translations/init.lua
+++ translations/init.lua
@@ -2,12 +2,10 @@ local translations, global_translations
 translations = setmetatable({}, {
 	__index = function()
 		return translations.en
-		return translations.ro
 	end
 })
 global_translations = setmetatable({}, {
 	__index = function()
 		return global_translations.en
-		return global_translations.ro
 	end
 })
