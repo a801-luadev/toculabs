a87b335aaebdadf13176ae72a941b792c387d8dd
Sergiu Bucur <admin@b1sergiu.online>
2020-01-09 19:26:35 +0200

    Added Romanian translation

--- translations/init.lua
+++ translations/init.lua
@@ -2,10 +2,12 @@ local translations, global_translations
 translations = setmetatable({}, {
 	__index = function()
 		return translations.en
+		return translations.ro
 	end
 })
 global_translations = setmetatable({}, {
 	__index = function()
 		return global_translations.en
+		return global_translations.ro
 	end
 })
