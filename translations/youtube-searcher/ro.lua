86d45759bbe720adbf75aa4958933d4e3bb3e6d4
Sergiu Bucur <admin@b1sergiu.online>
2020-01-09 19:24:17 +0200

    Create ro.lua

---
+++ translations/youtube-searcher/ro.lua
@@ -0,0 +1,12 @@
+translations.ro = setmetatable({
+	cant_search = "<r>Nu poți căuta încă. Așteaptă până când căutarea din urmă este încărcată complet.",
+	scheduled = "<j>Căutarea ta a fost adăugată în listă.",
+	now_searching = "<j>Se caută <d>%s</d>. Cerută de <d>%s</d>",
+	failure_searching = "<r>A apărut o eroare. Te rugăm să încerci din nou într-un minut.",
+	internal_error = "<r>Eroare internă.",
+	video_info = "<j>TitluȘ <d>%s</d>\nLink: <d>%s</d>\nCanal: <d>%s</d>\nDescriere: <d>%s</d>\nThumbnail: <d>%s</d>",
+	not_loaded = "<r>neîncărcat</r>",
+	click_to_search = "Apasă aici pentru a căuta"
+}, {
+	__index = global_translations.ro
+})
