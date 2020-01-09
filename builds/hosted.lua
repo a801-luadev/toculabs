04142dac09060e19c4c60559439afc3be01a0cac
Auto Hoster <action@github.com>
2020-01-09 18:46:26 +0000

    Update hosted file

--- builds/hosted.lua
+++ builds/hosted.lua
@@ -88,6 +88,37 @@ Folosește [<d>!help</d>] pentru mai multe informații.
 }
 translations.ro = global_translations.ro
 
+--[[ File translations/global/es.lua ]]--
+global_translations.es = {
+	welcome = [[<vp>¡Bienvenido a <b>Tocu Laboratories</b>!
+Contribuye en GitHub: <b>%s</b>
+Usa [<d>!help</d>] para obtener más información.
+• Entra a nustro servidor de Discord oficial para obtener ayuda con Lua y módulos: <b>https://discord.gg/quch83R</b>
+<d>Tip: Quizás quieras ver esta sala: <b>#%s</b>.</d>]],
+
+	lang_set = "<j>Lenguaje del módulo cambiado a: [<d>Español (es)</d>].",
+	current_lang = "<j>Lenguaje actual del módulo: [<d>Español (es)</d>].",
+
+	help = "<vp>¿Qué es <b>Tocu Laboratories</b>?\n<j><d><b>Tocu Laboratories</b></d> es un módulo alojado por <d><b>Tocutoeltuco#0000</b></d>, usado para publicar y probar cosas. Podés ver la lista de proyectos/submodos escribiendo <d><b>!modes</b></d> en el chat.\n<vp>¿Puedo alojar algo en el módulo?\n<j>Claro que puedes. Debes ir a nuestro repositorio de Github (<d><b>%s</b></d>), copiarlo y añadir tu módulo ahí. Encontrarás informacion en el archivo README.\n<vp>¿Cuáles son los comandos disponibles?\n<j>Podés usar <d><b>!commands</b></d> para verlos.",
+	use_info = "<d>Tip: Usa [<j>!info</j>] para obtener información de este submodo.</d>",
+	help_command_help = "Muestra información sobre Tocu Laboratories. También podes usar [<d>!help [comando]</d>] para obtener más informacion acerca de un comando.",
+	help_command_modes = "Muestra los submodos disponibles.",
+	help_command_info = "Muestra información sobre el submodo actual, si hay alguna.",
+	help_command_lang = "Cambia tu lenguaje en el módulo.",
+	help_command_pw = "Cambia la contraseña de la sala si es posible.",
+	help_command_commands = "Muestra la lista de comandos disponibles.",
+	no_help_provided = "<r>El comando [<d>!%s</d>] no tiene ningun texto de ayuda.",
+
+	cant_set_pw = "<r>No se pudo cambiar la contraseña.",
+
+	unknown_command = "<r>Comando desconocido [<d>!%s</d>].",
+	invalid_syntax = "<r>Estructura inválida. Usa [<d>!help %s</d>] para obtener más información del comando.",
+
+	new_password = "Nueva contraseña de la sala: [<d>%s</d>].",
+	password_removed = "La contraseña de la sala fue eliminada."
+}
+translations.es = global_translations.es
+
 
 --[[ File global/event-handler.lua ]]--
 local onEvent
@@ -516,6 +547,19 @@ else
 			__index = global_translations.ro
 		})
 
+		--[[ File translations/youtube-searcher/es.lua ]]--
+		translations.es = setmetatable({
+			cant_search = "<r>No se puede hacer una busqueda aún. Espera a que la actual termine de cargar.",
+			scheduled = "<j>Tu busqueda fue añadida a la lista de espera.",
+			now_searching = "<j>Buscando <d>%s</d>. Pedido por <d>%s</d>",
+			failure_searching = "<r>Pasó algo en los sistemas. Por favor intentalo de nuevo en un minuto.",
+			internal_error = "<r>Error interno.",
+			video_info = "<j>Título del video: <d>%s</d>\nLink: <d>%s</d>\nCanal: <d>%s</d>\nDescripción: <d>%s</d>\nMiniatura: <d>%s</d>",
+			not_loaded = "<r>sin cargar</r>",
+			click_to_search = "Clic aquí para buscar"
+		}, {
+			__index = global_translations.es
+		})
 		--[[ File youtube-searcher/room-gateway.lua ]]--
 		--[[
 			Here's how it does work:
