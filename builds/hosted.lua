bdb98df8ba21d8e8672fceadf37c6f753b1b5e16
Auto Hoster <action@github.com>
2020-01-10 23:46:26 +0000

    Update hosted file

--- builds/hosted.lua
+++ builds/hosted.lua
@@ -540,7 +540,7 @@ else
 			now_searching = "<j>Se caută <d>%s</d>. Cerută de <d>%s</d>",
 			failure_searching = "<r>A apărut o eroare. Te rugăm să încerci din nou într-un minut.",
 			internal_error = "<r>Eroare internă.",
-			video_info = "<j>TitluȘ <d>%s</d>\nLink: <d>%s</d>\nCanal: <d>%s</d>\nDescriere: <d>%s</d>\nThumbnail: <d>%s</d>",
+			video_info = "<j>Titlu: <d>%s</d>\nLink: <d>%s</d>\nCanal: <d>%s</d>\nDescriere: <d>%s</d>\nThumbnail: <d>%s</d>",
 			not_loaded = "<r>neîncărcat</r>",
 			click_to_search = "Apasă aici pentru a căuta"
 		}, {
