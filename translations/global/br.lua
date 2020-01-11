0d2653af5b29d01eb26ab30cfaaf05630e1a4ef8
Lautenschlager <tai.romani@hotmail.com>
2020-01-11 12:01:55 -0300

    Add br translation

---
+++ translations/global/br.lua
@@ -0,0 +1,29 @@
+global_translations.br = {
+	welcome = [[<vp>Bem-vindo aos <b>Laboratórios Tocu</b>!
+Contribua no Github: <b>%s</b>
+Use [<d>!help</d>] para obter mais informações.
+• Entre em nosso servidor oficial no Discord para conseguir ajuda em Lua e modules: <b>https://discord.gg/quch83R</b>
+<d>Dica: Você também vai querer checar essa sala: <b>#%s</b>.</d>]],
+
+	lang_set = "<j>Idioma do módulo definido para: [<d>Inglês (en)</d>].",
+	current_lang = "<j>Idioma atual do módulo: [<d>Inglês (en)</d>].",
+
+	help = "<vp>O que são os <b>Laboratórios Tocu</b>?\n<j><d><b>Laboratórios Tocu</b></d> fazem parte de um módulo hospedado por <d><b>Tocutoeltuco#0000</b></d>, usado para publicar e testar coisas. Você pode ver a lista de projetos/submodos digitando <d><b>!modes</b></d> no chat.\n<vp>Posso hospedar algo no módulo?\n<j>É claro que você pode. Você precisa ir até nosso repositório no Github (<d><b>%s</b></d>), clicar em fork fork e adicionar seu módulo lá. Você irá encontrar informações no arquivo README.\n<vp>Quais são os comandos disponíveis?\n<j>Você pode usar <d><b>!commands</b></d> para vê-los.",
+	use_info = "<d>Dica: Use [<j>!info</j>] para obter mais informações sobre esse módulo.</d>",
+	help_command_help = "Mostra informações sobre os Laboratórios Tocu. Você também pode usar [<d>!help [comando]</d>] para obter mais informações sobre um comando.",
+	help_command_modes = "Mostra os submodos disponíveis.",
+	help_command_info = "Mostra informações sobre o submodo atul, se houver.",
+	help_command_lang = "Define o idioma do módulo para você.",
+	help_command_pw = "Define uma senha para a sala, se possível.",
+	help_command_commands = "Mostra os comandos disponíveis.",
+	no_help_provided = "<r>O comando [<d>!%s</d>] não tem texto de ajuda.",
+
+	cant_set_pw = "<r>Não é possível definir uma senha.",
+
+	unknown_command = "<r>Comando desconhecido [<d>!%s</d>].",
+	invalid_syntax = "<r>Formato inválido. Use [<d>!help %s</d>] para obter mais informações.",
+
+	new_password = "Nova senha da sala: [<d>%s</d>].",
+	password_removed = "A senha sala foi removida."
+}
+translations.br = global_translations.br
