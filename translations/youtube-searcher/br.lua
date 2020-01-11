b4ce0ffdf36bd8232c052071705f2b241c2685be
Lautenschlager <tai.romani@hotmail.com>
2020-01-11 12:13:49 -0300

    add br translation

---
+++ translations/youtube-searcher/br.lua
@@ -0,0 +1,12 @@
+translations.br = setmetatable({
+	cant_search = "<r>Não é possível fazer uma procura ainda. Aguarde até que a atual tenha carregado completamente.",
+	scheduled = "<j>Sua pesquisa foi adicionada à fila.",
+	now_searching = "<j>Pesquisando <d>%s</d>. Requisitado por <d>%s</d>",
+	failure_searching = "<r>Algo aconteceu com os sistemas. Por favor, tente novamente mais tarde.",
+	internal_error = "<r>Erro interno.",
+	video_info = "<j>Título do vídeo: <d>%s</d>\nLink: <d>%s</d>\nCanal: <d>%s</d>\nDescrição: <d>%s</d>\nThumbnail: <d>%s</d>",
+	not_loaded = "<r>indisponível no momento</r>",
+	click_to_search = "Clique aqui para pesquisar"
+}, {
+	__index = global_translations.br
+})
