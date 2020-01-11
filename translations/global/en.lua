129c8319999496cd8f6db00a4769ef88c7c04420
Lautenschlager <tai.romani@hotmail.com>
2020-01-11 12:19:15 -0300

    Add BR translation (#6)
    * Add br translation
    
    * improve en texts
    
    * add br translation

--- translations/global/en.lua
+++ translations/global/en.lua
@@ -3,18 +3,18 @@ global_translations.en = {
 Contribute on GitHub: <b>%s</b>
 Use [<d>!help</d>] to get more information.
 • Join our official Discord server to get help with Lua and modules: <b>https://discord.gg/quch83R</b>
-<d>Tip: You'd also want to check this room: <b>#%s</b>.</d>]],
+<d>Tip: You may also want to check this room: <b>#%s</b>.</d>]],
 
 	lang_set = "<j>Module language set to: [<d>English (en)</d>].",
 	current_lang = "<j>Current module language: [<d>English (en)</d>].",
 
-	help = "<vp>What is <b>Tocu Laboratories</b>?\n<j><d><b>Tocu Laboratories</b></d> is a module hosted by <d><b>Tocutoeltuco#0000</b></d>, used to publish and test stuff. You can see the project/submode list by typing <d><b>!modes</b></d> in the chat.\n<vp>Can I host something in the module?\n<j>Of course you can. You need to go to our Github repository (<d><b>%s</b></d>), fork it and add your module here. You'll find info in the README file.\n<vp>What are the available commands?\n<j>You can use <d><b>!commands</b></d> to see them.",
+	help = "<vp>What is <b>Tocu Laboratories</b>?\n<j><d><b>Tocu Laboratories</b></d> is a module hosted by <d><b>Tocutoeltuco#0000</b></d>, used to publish and test stuff. You can see the projects'/submodes' list by typing <d><b>!modes</b></d> in the chat.\n<vp>Can I host something in the module?\n<j>Of course you can. You need to go to our Github repository (<d><b>%s</b></d>), fork it and add your module here. You'll find info in the README file.\n<vp>What are the available commands?\n<j>You can use <d><b>!commands</b></d> to see them.",
 	use_info = "<d>Tip: Use [<j>!info</j>] to get information about this submode.</d>",
 	help_command_help = "Shows information about Tocu Laboratories. You can also use [<d>!help [command]</d>] to get more information about a command.",
 	help_command_modes = "Shows available submodes.",
 	help_command_info = "Shows information about the current submode, if any.",
 	help_command_lang = "Sets your module language.",
-	help_command_pw = "Sets the room password if possible.",
+	help_command_pw = "Sets the room password, if possible.",
 	help_command_commands = "Shows the available commands.",
 	no_help_provided = "<r>The command [<d>!%s</d>] doesn't have any help text.",
 
