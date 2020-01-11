3d4cfc1f1883baf6801127dcaa79d1da0a8dfdd5
FilmCraft <54555281+FilmCraft@users.noreply.github.com>
2020-01-11 14:40:13 +0200

    Create he.lua

---
+++ translations/youtube-searcher/he.lua
@@ -0,0 +1,12 @@
+translations.he = setmetatable({
+	cant_search = "<r>לא ניתן לבצע חיפוש כרגע. חכו עד שהחיפוש הנוכחי נגמר לגמרי.",
+	scheduled = "<j>החיפוש שלכם נוסף לתור.",
+	now_searching = "<j>מחפש <d>%s</d>. בוקש על ידי <d>%s</d>",
+	failure_searching = "<r>משהו קרה במערכת. נסו לחפש שוב מאוחר יותר.",
+	internal_error = "<r>שגיאה פנימית.",
+	video_info = "<j>כותרת הסרטון: <d>%s</d>\nקישור: <d>%s</d>\nערוץ: <d>%s</d>\nתיאור: <d>%s</d>\nתמונה: <d>%s</d>",
+	not_loaded = "<r>לא נטען</r>",
+	click_to_search = "לחצו כאן מכדי לחפש"
+}, {
+	__index = global_translations.he
+})
